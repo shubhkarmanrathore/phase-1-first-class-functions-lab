7de276ff2ab19c24f01ed6b40f785a450f54d7e0
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-03-17 17:31:15 -0700

    adds first class func lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers() {
+    return drivers.slice(0,2)
+}
+
+function returnLastTwoDrivers() {
+    return drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare*multiplier
+    }
+}
+
+function fareDoubler(fare) {
+    const doubleFare = createFareMultiplier(2);
+    return doubleFare(fare);
+  }
+ 
+  function fareTripler(fare) {
+    const tripleFare = createFareMultiplier(3);
+    return tripleFare(fare);
+  }
+
+
+function selectDifferentDrivers(arrayOfDrivers, driverSelectorFunction) {
+    return driverSelectorFunction(arrayOfDrivers);
+  }
